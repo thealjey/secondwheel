89521395ec465ebb86a2850c52529062ee28e0fb
Eugene Kuzmenko <eugene.kuzmenko@vyzvoice.com>
2019-02-05 15:03:10 +0100

    feat(maskPassword): adds maskPassword

---
+++ maskPassword.js
@@ -0,0 +1,17 @@
+/* @flow */
+
+const repeat = require('lodash/repeat')
+
+/**
+ * produces a string of the same length as the original with every character
+ * replaced by a `"*"`
+ *
+ * @example
+ * import maskPassword from 'secondwheel/maskPassword'
+ *
+ * console.log(maskPassword('secret'))
+ * // => ******
+ */
+const maskPassword = (value/*: string */ = '') => repeat('*', value.length)
+
+module.exports = maskPassword
---
+++ test/maskPassword.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+const { describe, it } = require('mocha')
+const maskPassword = require('../maskPassword')
+const { strictEqual } = require('assert')
+
+describe('maskPassword', () => {
+  it('verify result', () => {
+    strictEqual(maskPassword('super secret password'), '*********************')
+  })
+})
