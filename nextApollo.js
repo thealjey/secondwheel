2abc5a63a6f9261be49c5b8d819bb4d045cc2483
Eugene Kuzmenko <eugene.kuzmenko@vyzvoice.com>
2019-02-05 13:54:05 +0100

    feat(nextApollo): adds the nextApollo function
    an Apollo-Client HOC for the Next.js App component

---
+++ nextApollo.js
@@ -0,0 +1,90 @@
+/* @flow */
+
+const { createElement, PureComponent } = require('react')
+const Head = require('next/head').default
+const { getDataFromTree } = require('react-apollo')
+const { isNode } = require('./constants')
+const noop = require('lodash/noop')
+
+/*::
+import ApolloClient from 'apollo-client'
+import App from 'next/app'
+*/
+
+/**
+ * Apollo-Client HOC for the Next.js
+ * {@link https://nextjs.org/docs/#custom-app|App} component.
+ *
+ * A simpler way to achieve the same result as in
+ * {@link https://github.com/zeit/next.js/tree/master/examples/with-apollo|with-apollo},
+ * which does not require any boilerplate code.
+ *
+ * The `ctx` object is equivalent to the one received in all
+ * {@link https://nextjs.org/docs/#fetching-data-and-component-lifecycle|getInitialProps}
+ * hooks.
+ *
+ * @example
+ * import React from 'react'
+ * import App, { Container } from 'next/app'
+ * import { ApolloProvider } from 'react-apollo'
+ * import nextApollo from 'secondwheel/nextApollo'
+ * import ApolloClient from 'secondwheel/ApolloClient'
+ *
+ * class MyApp extends App {
+ *   render () {
+ *     const { Component, pageProps, client } = this.props
+ *
+ *     return (
+ *       <Container>
+ *         <ApolloProvider client={client}>
+ *           <Component {...pageProps} />
+ *         </ApolloProvider>
+ *       </Container>
+ *     )
+ *   }
+ * }
+ *
+ * export default nextApollo(
+ *   options => new ApolloClient({ uri: 'http://localhost:4000', ...options })
+ * )(MyApp)
+ */
+const nextApollo = (
+  getClient/*: (options: Object, ctx?: Object) => ApolloClient */,
+  getProps/*: (ctx: Object) => ?Object */ = noop
+) => (app/*: App */) => class extends PureComponent/*:: <any> */ {
+  /*:: client: ApolloClient; */
+
+  static async getInitialProps (config/*: Object */) {
+    const { Component, router, ctx } = config
+    const props = {
+      ...(app.getInitialProps ? await app.getInitialProps(config) : {}),
+      ...getProps(ctx)
+    }
+    const client = getClient({}, ctx)
+
+    if (isNode) {
+      try {
+        await getDataFromTree(
+          createElement(app, { ...props, Component, router, client })
+        )
+      } catch (error) {
+        console.error('Error while running `getDataFromTree`', error)
+      }
+      Head.rewind()
+    }
+
+    return { ...props, cache: client.cache.extract() }
+  }
+
+  constructor (props/*: Object */) {
+    super(props)
+
+    this.client = getClient({ cache: props.cache })
+  }
+
+  render () {
+    return createElement(app, { ...this.props, client: this.client })
+  }
+}
+
+module.exports = nextApollo
---
+++ test/nextApollo.js
@@ -0,0 +1,48 @@
+/* @flow */
+
+const { describe, it } = require('mocha')
+const App = require('next/app').default
+const ApolloClient = require('../ApolloClient')
+const constant = require('lodash/constant')
+const proxyquire = require('proxyquire')
+const { stub } = require('sinon')
+const { ok } = require('assert')
+const { createElement } = require('react')
+const { renderToString } = require('react-dom/server')
+
+const Component = constant(null)
+const testError = new Error('test error')
+
+const req = options => proxyquire('../nextApollo', options)
+
+describe('nextApollo', () => {
+  it('app', () => {
+    const nextApollo = req({})
+    const MyApp = nextApollo(
+      options => new ApolloClient(options),
+      ctx => ({ test: 'test' })
+    )(App)
+    renderToString(createElement(MyApp, { ...MyApp.getInitialProps({ Component, router: {} }), Component, router: {} }))
+  })
+
+  it('empty component', () => {
+    const error = stub(console, 'error')
+
+    const nextApollo = req({
+      'react-apollo': {
+        getDataFromTree () {
+          throw testError
+        }
+      }
+    })
+    const MyApp = nextApollo(
+      options => new ApolloClient(options),
+      ctx => ({ test: 'test' })
+    )(Component)
+    renderToString(createElement(MyApp, { ...MyApp.getInitialProps({ Component, router: {} }), Component, router: {} }))
+
+    ok(error.calledOnceWithExactly('Error while running `getDataFromTree`', testError))
+
+    error.restore()
+  })
+})
